65055eef77689bfda71bd13515384c72705f5c6d
jiashubing <1310059060@qq.com>
2016-11-26 14:09:31 +0800

    增加转盘的后台实现

---
+++ test/src/common/controller/HomeAction.java
@@ -0,0 +1,90 @@
+package common.controller;
+
+import java.util.Random;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import common.entity.Result;
+
+import config.ValidatePcMobile;
+
+@Controller
+public class HomeAction {
+	
+	@RequestMapping("/index")
+	public String loadIndex(Model model,HttpServletRequest request)throws Exception{
+		return ValidatePcMobile.checkRequest(request, "/index");
+	}
+	
+	@RequestMapping("/lottery")
+    @ResponseBody
+    public Result loadLottery() {
+        Result result = new Result();
+        Object[][] prizeArr = new  Object[][]{
+				//id,min,max，prize【奖项】,v【中奖率】
+				//里面的指针转动（没有）
+				//外面的转盘转动
+				{1,1,14,"恭喜你获得一等奖",1},
+				{2,346,364,"恭喜你获得一等奖",1},
+				{3,16,44,"不要灰心",10},
+				{4,46,74,"恭喜你中了一毛钱",10},
+				{5,76,104,"祝您好运",10},
+				{6,106,134,"恭喜你获得二等奖",2},
+				{7,136,164,"再接再厉",10},
+				{8,166,194,"恭喜你中了一个亿",10},
+				{9,196,224,"运气先攒着",10},
+				{10,226,254,"恭喜你获得三等奖",5},
+				{11,256,284,"要加油哦",10},
+				{12,286,314,"恭喜你中了一千块",10},
+				{13,316,344,"谢谢参与",10}
+		};
+        Object ans[] = award(prizeArr);//抽奖后返回角度和奖品等级
+        result.setStatus(1);
+        result.setBody(ans[0]);
+        result.setMessage((String)ans[2]);
+
+        return result;
+    }
+	
+	//抽奖并返回角度和奖项
+		public Object[] award(Object[][] prizeArr){
+			//概率数组
+			Integer obj[] = new Integer[prizeArr.length];
+			for(int i=0;i<prizeArr.length;i++){
+				obj[i] = (Integer) prizeArr[i][4];
+			}
+			Integer prizeId = getRand(obj); //根据概率获取奖项id
+			//旋转角度
+			int angle = new Random().nextInt((Integer)prizeArr[prizeId][2]-(Integer)prizeArr[prizeId][1])+(Integer)prizeArr[prizeId][1];
+			String msg = (String) prizeArr[prizeId][3];//提示信息
+			return new Object[]{angle,prizeId,msg};
+		}
+		//根据概率获取奖项
+		public Integer getRand(Integer obj[]){
+			Integer result = null;
+			try {
+				int  sum = 0;//概率数组的总概率精度 
+				for(int i=0;i<obj.length;i++){
+					sum+=obj[i];
+				}
+				for(int i=0;i<obj.length;i++){//概率数组循环 
+					int randomNum = new Random().nextInt(sum);//随机生成1到sum的整数
+					if(randomNum<obj[i]){//中奖
+						result = i;
+						break;
+					}else{
+						sum -=obj[i];
+					}
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			return result;
+		}
+	
+}
---
+++ test/src/common/entity/Result.java
@@ -0,0 +1,114 @@
+package common.entity;
+
+import java.io.Serializable;
+import java.util.Hashtable;
+import java.util.List;
+
+/**
+ * Created by shiliting on 2015/6/25.
+ * 该类是结果对象
+ * @author shiliting
+ */
+public  class Result implements Serializable {
+
+    private static final long serialVersionUID = -349012453592429794L;
+    private int status;//状态信息
+    private String message;//消息信息
+    private Object body;//主体
+    private int error;
+    private String url;
+    private String moveup_dir_path;
+    private String current_dir_path;
+    private String current_url;
+    private int total_count;
+    private List<Hashtable> file_list;
+
+    public Result() {
+
+    }
+
+    public Result(int status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+
+    public String getMoveup_dir_path() {
+        return moveup_dir_path;
+    }
+
+    public void setMoveup_dir_path(String moveup_dir_path) {
+        this.moveup_dir_path = moveup_dir_path;
+    }
+
+    public String getCurrent_dir_path() {
+        return current_dir_path;
+    }
+
+    public void setCurrent_dir_path(String current_dir_path) {
+        this.current_dir_path = current_dir_path;
+    }
+
+    public String getCurrent_url() {
+        return current_url;
+    }
+
+    public void setCurrent_url(String current_url) {
+        this.current_url = current_url;
+    }
+
+    public int getTotal_count() {
+        return total_count;
+    }
+
+    public void setTotal_count(int total_count) {
+        this.total_count = total_count;
+    }
+
+    public List<Hashtable> getFile_list() {
+        return file_list;
+    }
+
+    public void setFile_list(List<Hashtable> file_list) {
+        this.file_list = file_list;
+    }
+
+    public int getError() {
+        return error;
+    }
+
+    public void setError(int error) {
+        this.error = error;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Object getBody() {
+        return body;
+    }
+
+    public void setBody(Object body) {
+        this.body = body;
+    }
+}
