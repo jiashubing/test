361f736f6c50aaeb3506786e90498d8ca368a5fa
jiashubing <1310059060@qq.com>
2016-11-27 13:35:30 +0800

    注释测试

--- test/src/forum/interceptor/LoginInterceptor.java
+++ test/src/forum/interceptor/LoginInterceptor.java
@@ -1,75 +1,75 @@
-package jiabin.interceptor;
-
-import java.util.Map;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
-
-import jiabin.util.StringUtil;
-
-import org.apache.struts2.ServletActionContext;
-import org.jboss.logging.Logger;
-
-import com.opensymphony.xwork2.ActionInvocation;
-import com.opensymphony.xwork2.interceptor.MethodFilterInterceptor;
-
-public class LoginInterceptor extends MethodFilterInterceptor {
-
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-
-	private static final Logger log=Logger.getLogger(LoginInterceptor.class);
-	
-	public final static String SESSION_KEY="regUserId";
-	//public final static String GOTO_URL_KEY="GOING_TO";
-	
-	@Override
-	protected String doIntercept(ActionInvocation invocation) throws Exception {
-		HttpServletRequest request=ServletActionContext.getRequest();
-		HttpSession session=request.getSession();
-		
-		if (session!=null&&session.getAttribute("currentUser")!=null) {
-			return invocation.invoke();
-		}
-		
-		setGoingURL(request, session, invocation);
-		return invocation.invoke();
-	}
-	
-	private void setGoingURL(HttpServletRequest request,HttpSession session,ActionInvocation invocation){
-		//Èç¹ûrefer²»Îª¿Õ£¬Ö±½ÓÊ¹ÓÃËü¡£Èç¹ûÎª¿ÕÎÒÃÇ·Ö±ð»ñµÃÃüÃû¿Õ¼ä£¬actionÃû£¬ÒÔ¼°ÇëÇó²ÎÊý£¬ÖØÐÂ¹¹Ôì³ÉÒ»¸öURL±£´æÔÚSESSIONÖÐ
-		String url=request.getHeader("referer");
-		String url2;
-		if (url==null||url.equals("")) {
-			url="";
-			String path=request.getContextPath();
-			String actionName=invocation.getProxy().getActionName();
-			String nameSpace=invocation.getProxy().getNamespace();
-			if (StringUtil.isNotEmpty(nameSpace)) {
-				url=url+nameSpace;
-			}
-			if (StringUtil.isNotEmpty(actionName)) {
-				url=url+"/"+actionName+".action"+"?";
-			}
-			Map<String,String[]> zzMap=request.getParameterMap();
-			if (zzMap!=null) {
-				for (String s : zzMap.keySet()) {
-					String[] value=zzMap.get(s);
-					for (String val : value) {
-						url=url+s+"="+val+"&";
-					}
-				}
-			}
-		}
-		if (url.indexOf("prePage")>0) {
-			url2=StringUtil.getParamFromUrl(url);
-			session.setAttribute("prepareGoingTo", url2);
-			session.setAttribute("backGoingTo", url);
-		}else {
-			session.setAttribute("GOING_TO", url);
-		}
-	}
-
-}
+//package jiabin.interceptor;
+//
+//import java.util.Map;
+//
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpSession;
+//
+//import jiabin.util.StringUtil;
+//
+//import org.apache.struts2.ServletActionContext;
+//import org.jboss.logging.Logger;
+//
+//import com.opensymphony.xwork2.ActionInvocation;
+//import com.opensymphony.xwork2.interceptor.MethodFilterInterceptor;
+//
+//public class LoginInterceptor extends MethodFilterInterceptor {
+//
+//	/**
+//	 * 
+//	 */
+//	private static final long serialVersionUID = 1L;
+//
+//	private static final Logger log=Logger.getLogger(LoginInterceptor.class);
+//	
+//	public final static String SESSION_KEY="regUserId";
+//	//public final static String GOTO_URL_KEY="GOING_TO";
+//	
+//	@Override
+//	protected String doIntercept(ActionInvocation invocation) throws Exception {
+//		HttpServletRequest request=ServletActionContext.getRequest();
+//		HttpSession session=request.getSession();
+//		
+//		if (session!=null&&session.getAttribute("currentUser")!=null) {
+//			return invocation.invoke();
+//		}
+//		
+//		setGoingURL(request, session, invocation);
+//		return invocation.invoke();
+//	}
+//	
+//	private void setGoingURL(HttpServletRequest request,HttpSession session,ActionInvocation invocation){
+//		//ï¿½ï¿½ï¿½referï¿½ï¿½Îªï¿½Õ£ï¿½Ö±ï¿½ï¿½Ê¹ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½Îªï¿½ï¿½ï¿½ï¿½ï¿½Ç·Ö±ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½Õ¼ä£¬actionï¿½ï¿½ï¿½Ô¼ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½Â¹ï¿½ï¿½ï¿½ï¿½Ò»ï¿½ï¿½URLï¿½ï¿½ï¿½ï¿½ï¿½ï¿½SESSIONï¿½ï¿½
+//		String url=request.getHeader("referer");
+//		String url2;
+//		if (url==null||url.equals("")) {
+//			url="";
+//			String path=request.getContextPath();
+//			String actionName=invocation.getProxy().getActionName();
+//			String nameSpace=invocation.getProxy().getNamespace();
+//			if (StringUtil.isNotEmpty(nameSpace)) {
+//				url=url+nameSpace;
+//			}
+//			if (StringUtil.isNotEmpty(actionName)) {
+//				url=url+"/"+actionName+".action"+"?";
+//			}
+//			Map<String,String[]> zzMap=request.getParameterMap();
+//			if (zzMap!=null) {
+//				for (String s : zzMap.keySet()) {
+//					String[] value=zzMap.get(s);
+//					for (String val : value) {
+//						url=url+s+"="+val+"&";
+//					}
+//				}
+//			}
+//		}
+//		if (url.indexOf("prePage")>0) {
+//			url2=StringUtil.getParamFromUrl(url);
+//			session.setAttribute("prepareGoingTo", url2);
+//			session.setAttribute("backGoingTo", url);
+//		}else {
+//			session.setAttribute("GOING_TO", url);
+//		}
+//	}
+//
+//}
